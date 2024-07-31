7d520d4ace8dc69a72a15348205d84d19701fd10
JITENDRA PRAJAPATI <115032780+jitendraprajapati9956@users.noreply.github.com>
2024-07-31 21:55:05 +0530

    Update 2.php

--- Lab1/2.php
+++ Lab1/2.php
@@ -2,10 +2,11 @@
     <head></head>
     <body>
         <?php
+          # Create PHP script which prints Name , Roll Number & sem 2 SPI on the web page.
             echo "<h1>";
             echo "Name : Jitendra Prajapati <br>";
             echo "RollNo : MA061 <br>";
-            echo "Sem 2 SPI : 7.0<br>";
+            echo "Sem 2 SPI : 6.08<br>";
             echo "</h1>";
         ?>
     </body>
