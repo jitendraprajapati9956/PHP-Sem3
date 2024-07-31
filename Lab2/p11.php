edf2b3d2ab7f57685d570b7076e76debab38890a
JITENDRA PRAJAPATI <115032780+jitendraprajapati9956@users.noreply.github.com>
2024-07-31 23:55:53 +0530

    Update p11.php

--- Lab2/p11.php
+++ Lab2/p11.php
@@ -1,4 +1,7 @@
 <?php
+echo "<h5>Create a php script which checks whether the number is palindrome or not.
+E.d 121 is a palindrome number.
+</h5>";
 function isPalindrome($number) {
     $numberString = strval($number);
     $reverseString = strrev($numberString);
@@ -6,5 +9,8 @@ function isPalindrome($number) {
     return $numberString === $reverseString;
 }
 $number1 = 121;
+var_dump($number1);
+echo "<br>";
+echo "<br>";
 echo "$number1 is " . (isPalindrome($number1) ? "a palindrome" : "not a palindrome");
 ?>
