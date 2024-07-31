d45b0eb27021020b49babb35974df43885b87249
JITENDRA PRAJAPATI <115032780+jitendraprajapati9956@users.noreply.github.com>
2024-08-01 00:22:13 +0530

    Update 1.php

--- Lab4/1.php
+++ Lab4/1.php
@@ -1,4 +1,9 @@
 <?php
+echo "<h5>Create a php script which passes two values and the choice of operation as
+parameters of a function . Depending on the choice function should perform the
+arithmetic operation on values.
+</h5>";
+
 function oper($value1, $value2, $operation) {
     switch ($operation) {
         case '+':
@@ -19,9 +24,17 @@ function oper($value1, $value2, $operation) {
 }
 
 $value1 = 10;
+echo "<b>value1:</b>\n";
+echo $value1;
+echo "<br>";
 $value2 = 5;
+echo "<b>value2:</b>\n";
+echo $value2;
+echo "<br>";
+echo "<b>Operation:</b>\n";
 $operation = '-'; 
-
+echo $operation ."<br>";
+echo "<br>";
 $result = oper($value1, $value2, $operation);
 echo "Result: " . $result;
 ?>
