a97f8900c75f81bc6e940ace772abc450bda688e
JITENDRA PRAJAPATI <115032780+jitendraprajapati9956@users.noreply.github.com>
2024-07-31 23:35:42 +0530

    Update p2.php

--- Lab2/p2.php
+++ Lab2/p2.php
@@ -1,11 +1,21 @@
 
 <?php
-
+echo "<h5>Create two variables of integer type and display the value of the variables</h5>";
+  
 $iVar = 14;
 $fVar = 3.14;
 $sVar = "Hello world!";
 $bVar = true;
 
+var_dump($iVar);
+echo "<br>";
+var_dump($fVar);
+echo "<br>";
+var_dump($sVar);
+echo "<br>";
+var_dump($bVar);
+echo "<br>";
+echo "<br>";
 echo "Integer Variable:$iVar";
 echo "<br>";
 echo "Float Variable:$fVar";
