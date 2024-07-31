aedb1b6ea0885b47caba89d59167a3ad2b3bb185
JITENDRA PRAJAPATI <115032780+jitendraprajapati9956@users.noreply.github.com>
2024-07-31 23:52:05 +0530

    Update p7.php

--- Lab2/p7.php
+++ Lab2/p7.php
@@ -1,4 +1,7 @@
 <?php
+echo "<h5> Create a php script which demonstrates the scope of local variables. {display
+the appropriate error also in output by making changes into script)";
+
 function demonstrateLocalScope() {
     $localVar = "I am a local variable::";
     echo "Inside the function::$localVar";
