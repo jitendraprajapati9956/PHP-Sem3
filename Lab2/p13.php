f764b59af134bffce4bab44091f58144857d0606
JITENDRA PRAJAPATI <115032780+jitendraprajapati9956@users.noreply.github.com>
2024-08-01 00:01:32 +0530

    Update p13.php

--- Lab2/p13.php
+++ Lab2/p13.php
@@ -1,4 +1,13 @@
 <?php
+echo "<h5>Create a php script for given conditions.
+If age < 20 or age > 60 print message not valid
+age
+If age is between 20 to 35 , print message age is
+in range of 20 to 35
+If age is between 36 to 55 print message age is in
+range of 36 to 55
+Else print message age is more than 55
+</h5>";
 function checkAge($age) {
     if ($age < 20 || $age > 60) {
         echo "Not a valid age";
@@ -19,8 +28,19 @@ $age2 = 25;
 $age3 = 40;
 $age4 = 65;
 
+var_dump($age1);
+echo "<br>";
 checkAge($age1);
+echo "<br>";
+var_dump($age2);
+echo "<br>";
 checkAge($age2); 
+echo "<br>";
+var_dump($age3);
+echo "<br>";
 checkAge($age3); 
+echo "<br>";
+var_dump($age4);
+echo "<br>";
 checkAge($age4); 
 ?>
