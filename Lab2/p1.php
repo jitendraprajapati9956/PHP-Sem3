d656dcb727eede1a42596af37410fa83ee31ebc4
JITENDRA PRAJAPATI <115032780+jitendraprajapati9956@users.noreply.github.com>
2024-07-31 22:52:02 +0530

    Update p1.php

--- Lab2/p1.php
+++ Lab2/p1.php
@@ -1,7 +1,13 @@
 <?php
     $num1 = 10;
     $num2 = 20;
-
+    
+    echo "<h5>Create two variables of integer type and display the value of the variables</h5>";
+    var_dump($num1);
+    echo "<br>";
+    var_dump($num2);
+    echo "<br>";
+    
     echo "Number:$num1";
     echo "<br>";
     echo "Number:$num2";
