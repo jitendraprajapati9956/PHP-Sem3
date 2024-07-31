3769f90c81adf022d81de088a3db81b73377f924
JITENDRA PRAJAPATI <115032780+jitendraprajapati9956@users.noreply.github.com>
2024-08-01 00:05:21 +0530

    Update p14.php

--- Lab2/p14.php
+++ Lab2/p14.php
@@ -1,5 +1,7 @@
 <?php
 $backgroundColor = "yellow";
+
+
 ?>
 
 <!DOCTYPE html>
@@ -8,6 +10,8 @@ $backgroundColor = "yellow";
     <title>Dynamic Background Color</title>
 </head>
 <body>
+<h5>Create the php script which changes the background color of the div tag
+according to variable value.(hint div style=background-color:yellow;>)</h5>
     <div style="background-color: <?php echo $backgroundColor; ?>;">
         <h1>Heading</h1>
         <p>This is an example of changing the background color using PHP.</p>
