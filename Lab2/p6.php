d13b6745bd02dc344bace46613c4020bb175f6ad
JITENDRA PRAJAPATI <115032780+jitendraprajapati9956@users.noreply.github.com>
2024-07-31 23:46:40 +0530

    Update p6.php

--- Lab2/p6.php
+++ Lab2/p6.php
@@ -1,12 +1,20 @@
 <?php
+echo "<h5> Create a php script which performs string operations.
+</h5>";
     $str1 = "Hello";
     $str2 = "DDU";
 
+    var_dump($str1);
+    echo "<br>";
+    var_dump($str2);
+    echo "<br>";
+    echo "<br>";
+    
     $con = $str1.$str2;
 
     $str1 .= $str2;
 
-    echo "<b>Concatenation::$con<b>";
+    echo "<b>Concatenation::<b>$con";
     echo "<br>";
     echo "<b>Concatenation & Assighment::$str1<b>";
 
